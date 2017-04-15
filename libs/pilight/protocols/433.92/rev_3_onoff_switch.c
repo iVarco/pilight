6c1534d859176d0836975a463f226b868d02133e
Lennart Buhl <git@hackmate.de>
2017-04-15 17:16:11 +0200

    rev_3_onoff_switch working now

--- libs/pilight/protocols/433.92/rev_3_onoff_switch.c
+++ libs/pilight/protocols/433.92/rev_3_onoff_switch.c
@@ -31,25 +31,30 @@
 #include "rev_3_onoff_switch.h"
 
 #define PULSE_MULTIPLIER	3
-#define MIN_PULSE_LENGTH	124 // 253
-#define MAX_PULSE_LENGTH	132 // 269
-#define AVG_PULSE_LENGTH	128 // 264
-#define RAW_LENGTH			64  // = (8 analog + 4 digital + 4 sync) * 4
+#define MIN_PULSE_LENGTH	250 // 253
+#define MAX_PULSE_LENGTH	265 // 269
+#define AVG_PULSE_LENGTH	258 // 264
+#define RAW_LENGTH			50  // = (8 analog + 4 digital) * 4 + 2 sync
 
 static int validate(void) {
-	if(rev_3_onoff_switch->rawlen == RAW_LENGTH) {
+	int i;
+	for (i = 0; i < rev_3_onoff_switch->rawlen; i++) {
+		if (rev_3_onoff_switch->raw[i] == 0) // no value may be zero
+			return -1;
+	}
+	if (rev_3_onoff_switch->rawlen == RAW_LENGTH) {
 		if(rev_3_onoff_switch->raw[rev_3_onoff_switch->rawlen-1] >= (MIN_PULSE_LENGTH*PULSE_DIV) &&
-			rev_3_onoff_switch->raw[rev_3_onoff_switch->rawlen-1] <= (MAX_PULSE_LENGTH*PULSE_DIV)) {
+		   rev_3_onoff_switch->raw[rev_3_onoff_switch->rawlen-1] <= (MAX_PULSE_LENGTH*PULSE_DIV)) {
 			return 0;
 		}
 	}
 	return -1;
 }
 
-static void createMessage(int unit, int id, int state) {
+static void createMessage(int sys, int unit, int state) {
 	rev_3_onoff_switch->message = json_mkobject();
+	json_append_member(rev_3_onoff_switch->message, "sys", json_mknumber(sys, 0));
 	json_append_member(rev_3_onoff_switch->message, "unit", json_mknumber(unit, 0));
-	json_append_member(rev_3_onoff_switch->message, "id", json_mknumber(id, 0));
 	if(state == 1) {
 		json_append_member(rev_3_onoff_switch->message, "state", json_mkstring("on"));
 	} else {
@@ -57,87 +62,72 @@ static void createMessage(int unit, int id, int state) {
 	}
 }
 
-// DOESN'T WORK
+// TODO: NOT IMPLEMENTED
 static void parseCode(void) {
-	int x = 0, i = 0, binary[RAW_LENGTH/4];
-
-	/* Convert the one's and zero's into binary */
-	for(x=0;x<rev_3_onoff_switch->rawlen-2;x+=4) {
-		if(rev_3_onoff_switch->raw[x+3] > (int)((double)AVG_PULSE_LENGTH*((double)PULSE_MULTIPLIER/2))) {
-			binary[i++] = 1;
-		} else {
-			binary[i++] = 0;
-		}
-	}
-
-	int unit = binToDec(binary, 6, 9);
-	int id = binToDec(binary, 0, 5);
-	int state = binary[11];
-
-	createMessage(unit, id, state);
+	printf("Parsing for rev_3_onoff_switch is NOT IMPLEMENTED!\n");
+	//createMessage(system, unit, state);
 }
 
-// Note: 1 bit = 1024α
-
-static void create0(int first) { // length = 1024 pulses
-	rev_3_onoff_switch->raw[first]   = (AVG_PULSE_LENGTH);						// 128 high
-	rev_3_onoff_switch->raw[first+1] = (PULSE_MULTIPLIER*AVG_PULSE_LENGTH);		// 384 low
-	rev_3_onoff_switch->raw[first+2] = (AVG_PULSE_LENGTH);						// 128 high
-	rev_3_onoff_switch->raw[first+3] = (PULSE_MULTIPLIER*AVG_PULSE_LENGTH);		// 384 low
+static void create0(int start) { // always 4 ints
+	//printf("create 0 at %d\n", start);
+	rev_3_onoff_switch->raw[start]   = (AVG_PULSE_LENGTH);						// 128 high
+	rev_3_onoff_switch->raw[start+1] = (PULSE_MULTIPLIER*AVG_PULSE_LENGTH);		// 384 low
+	rev_3_onoff_switch->raw[start+2] = (AVG_PULSE_LENGTH);						// 128 high
+	rev_3_onoff_switch->raw[start+3] = (PULSE_MULTIPLIER*AVG_PULSE_LENGTH);		// 384 low
 }
 
-static void create1(int first) { // length = 1024 pulses
-	rev_3_onoff_switch->raw[first]   = (PULSE_MULTIPLIER*AVG_PULSE_LENGTH);		// 384 high
-	rev_3_onoff_switch->raw[first+1] = (AVG_PULSE_LENGTH);						// 128 low
-	rev_3_onoff_switch->raw[first+2] = (PULSE_MULTIPLIER*AVG_PULSE_LENGTH);		// 384 high
-	rev_3_onoff_switch->raw[first+3] = (AVG_PULSE_LENGTH);						// 128 low
+static void create1(int start) { // always 4 ints
+	//printf("create 1 at %d\n", start);
+	rev_3_onoff_switch->raw[start]   = (PULSE_MULTIPLIER*AVG_PULSE_LENGTH);		// 384 high
+	rev_3_onoff_switch->raw[start+1] = (AVG_PULSE_LENGTH);						// 128 low
+	rev_3_onoff_switch->raw[start+2] = (PULSE_MULTIPLIER*AVG_PULSE_LENGTH);		// 384 high
+	rev_3_onoff_switch->raw[start+3] = (AVG_PULSE_LENGTH);						// 128 low
 }
 
-static void createFloating(int first) { // length = 1024 pulses
-	rev_3_onoff_switch->raw[first]   = (AVG_PULSE_LENGTH);						// 128 high
-	rev_3_onoff_switch->raw[first+1] = (PULSE_MULTIPLIER*AVG_PULSE_LENGTH);		// 384 low
-	rev_3_onoff_switch->raw[first+2] = (PULSE_MULTIPLIER*AVG_PULSE_LENGTH);		// 384 high
-	rev_3_onoff_switch->raw[first+3] = (AVG_PULSE_LENGTH);						// 128 low
+static void createFloating(int start) { // always 4 ints
+	//printf("create F at %d\n", start);
+	rev_3_onoff_switch->raw[start]   = (AVG_PULSE_LENGTH);						// 128 high
+	rev_3_onoff_switch->raw[start+1] = (PULSE_MULTIPLIER*AVG_PULSE_LENGTH);		// 384 low
+	rev_3_onoff_switch->raw[start+2] = (PULSE_MULTIPLIER*AVG_PULSE_LENGTH);		// 384 high
+	rev_3_onoff_switch->raw[start+3] = (AVG_PULSE_LENGTH);						// 128 low
 }
 
-static void createSync(int first) { // length = 4096 pulses
-	rev_3_onoff_switch->raw[first]   = (AVG_PULSE_LENGTH);						// 128  high
-	rev_3_onoff_switch->raw[first+1] = (31*AVG_PULSE_LENGTH);					// 3968 low
+static void createSync(int start) { // always 2 ints
+	//printf("create sync at %d\n", start);
+	rev_3_onoff_switch->raw[start]   = (AVG_PULSE_LENGTH);						// 128  high
+	rev_3_onoff_switch->raw[start+1] = (34*AVG_PULSE_LENGTH);					// 3968 low
 }
 
 
 static void clearCode(void) {
-	// ???
-
-	// old:
-	//create1(0,3);
-	//create0(4,47);
-}
-
-static void createUnit(int unit) { // system code A0-A3, generates 4 bytes
-	int binary[255];
-	int length = 0, i = 0, x = 0;
-	length = decToBinRev(unit, binary);
-	for(i = 0; i <= length; i++) {
-		x = i * 4;
-		if (binary[i] == 1)
-			create1(x);
-		else if (binary[i] == 0)
-			createFloating(x);
-		else
-			printf("ERROR: Neither 0 nor 1\n");
+	// initialize with zeros
+	int i;
+	for (i = 0; i < rev_3_onoff_switch->rawlen; i++) {
+		rev_3_onoff_switch->raw[i] = 0;
 	}
 }
 
-static void createId(int id) { // switch code A4-A7, generates 4 bytes
+static void createSys(int sys) { // system code A0-A3, generates 4 bytes
+	int i;
+	for (i = 1; i <= 4; i++) {
+		int addr = (i-1) * 4;
+		if (sys == i)
+			create1(addr);
+		else
+			createFloating(addr);
+	}
+}
+
+static void createUnit(int unit) { // switch code A4-A7, generates 4 bytes
 	int start = 16, i;
 	for (i = 1; i <= 3; i++) {
-		if (id == i)
-			create1(start + (id-1)*4);
+		int addr = start + (i-1)*4;
+		if (unit == i)
+			create1(addr);
 		else
-			createFloating(start + (id-1)*4);
+			createFloating(addr);
 	}
-	create0(start + 12);
+	create0(start + 12); // A7 is ground
 }
 
 static void createState(int state) { // D3-D0, generates 4 bytes
@@ -159,39 +149,43 @@ static void createFooter(void) { // generate 4 bytes at the end
 
 // TODO
 static int createCode(struct JsonNode *code) {
+	int sys = -1;
 	int unit = -1;
-	int id = -1;
 	int state = -1;
+
 	double itmp = -1;
 
-	if(json_find_number(code, "unit", &itmp) == 0)
-		unit = (int)round(itmp);
+	if(json_find_number(code, "sys", &itmp) == 0)
+		sys = (int) round(itmp);
 
-	if(json_find_number(code, "id", &itmp) == 0)
-		id = (int)round(itmp);
+	if(json_find_number(code, "unit", &itmp) == 0)
+		unit = (int) round(itmp);
 
 	if(json_find_number(code, "off", &itmp) == 0)
 		state = 0;
 	else if(json_find_number(code, "on", &itmp) == 0)
 		state = 1;
 
-	if(unit == -1 || id == -1 || state == -1) {
+	if(sys == -1 || unit == -1 || state == -1) {
 		logprintf(LOG_ERR, "rev_3_onoff_switch: insufficient number of arguments");
 		return EXIT_FAILURE;
-	} else if(id > 63 || id < 0) {
-		logprintf(LOG_ERR, "rev_3_onoff_switch: invalid id range");
+	} else if(sys < 1 || sys > 4) {
+		logprintf(LOG_ERR, "rev_3_onoff_switch: invalid system id range");
 		return EXIT_FAILURE;
-	} else if(unit > 15 || unit < 0) {
-		logprintf(LOG_ERR, "rev_3_onoff_switch: invalid unit range");
+	} else if(unit < 0 || unit > 3) {
+		logprintf(LOG_ERR, "rev_3_onoff_switch: invalid unit id range");
 		return EXIT_FAILURE;
 	} else {
-		createMessage(unit, id, state);
+		createMessage(sys, unit, state);
 		clearCode();
-		createUnit(unit); // must generate 4 bytes (A0-A3)
-		createId(id); // must generate 4 bytes (A4-A7)
+		createSys(sys); // must generate 4 bytes (A0-A3)
+		createUnit(unit); // must generate 4 bytes (A4-A7)
 		createState(state); // must generate 4 bytes (D3-D0)
-		createFooter(); // must generate 1 byte (Sync)
+		createFooter(); // just creates sync
 		rev_3_onoff_switch->rawlen = RAW_LENGTH;
+		//int i; for (i=0; i<RAW_LENGTH; i++) {
+		//	printf("%3d ", rev_3_onoff_switch->raw[i]); if (i%4==3) printf ("\n");
+		//} printf ("\n");
 	}
 	return EXIT_SUCCESS;
 }
@@ -200,8 +194,8 @@ static int createCode(struct JsonNode *code) {
 static void printHelp(void) {
 	printf("\t -t --on\t\t\tsend an on signal\n");
 	printf("\t -f --off\t\t\tsend an off signal\n");
-	printf("\t -u --unit=unit\t\t\tcontrol a device with this unit code\n"); // system code (A0 - A3)
-	printf("\t -i --id=id\t\t\tcontrol a device with this id\n"); // power socket code (A4 - A6)
+	printf("\t -s --sys=sys\t\t\tspecify system code (A=1, B=2, C=3, D=4)\n"); // power socket code (A4 - A6)
+	printf("\t -u --unit=unit\t\t\tspecify unit code of receiver (1-3)\n"); // system code (A0 - A3)
 }
 
 #if !defined(MODULE) && !defined(_WIN32)
@@ -220,11 +214,10 @@ void rev3OnOffInit(void) {
 	rev_3_onoff_switch->maxgaplen = MAX_PULSE_LENGTH*PULSE_DIV;
 	rev_3_onoff_switch->mingaplen = MIN_PULSE_LENGTH*PULSE_DIV;
 
-	options_add(&rev_3_onoff_switch->options, 'u', "unit", OPTION_HAS_VALUE, DEVICES_ID, JSON_NUMBER, NULL, "^([0-9]|[1][0-5])$");
-	options_add(&rev_3_onoff_switch->options, 'i', "id", OPTION_HAS_VALUE, DEVICES_ID, JSON_NUMBER, NULL, "^(6[0123]|[12345][0-9]|[0-9]{1})$");
+	options_add(&rev_3_onoff_switch->options, 's', "sys", OPTION_HAS_VALUE, DEVICES_ID, JSON_NUMBER, NULL, "^([1-4]{1})$");
+	options_add(&rev_3_onoff_switch->options, 'u', "unit", OPTION_HAS_VALUE, DEVICES_ID, JSON_NUMBER, NULL, "^([1-4]{1})$");
 	options_add(&rev_3_onoff_switch->options, 't', "on", OPTION_NO_VALUE, DEVICES_STATE, JSON_STRING, NULL, NULL);
 	options_add(&rev_3_onoff_switch->options, 'f', "off", OPTION_NO_VALUE, DEVICES_STATE, JSON_STRING, NULL, NULL);
-
 
 	options_add(&rev_3_onoff_switch->options, 0, "readonly", OPTION_HAS_VALUE, GUI_SETTING, JSON_NUMBER, (void *)0, "^[10]{1}$");
 	options_add(&rev_3_onoff_switch->options, 0, "confirm", OPTION_HAS_VALUE, GUI_SETTING, JSON_NUMBER, (void *)0, "^[10]{1}$");
